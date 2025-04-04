5304e79e35367a14e7242ebdac547b4daa810f5b
beatrice <bbrownmulry@gmail.com>
2025-04-04 19:13:00 -0400

    replace logout server component with route handler

---
+++ frontend/app/user/logout/route.ts
@@ -0,0 +1,16 @@
+import { getCurrentSession, logoutUserAction } from "@/actions/users.server";
+import { UserDisplayData } from "@/schemas/fe.schema";
+import { redirect } from "next/navigation";
+import { NextResponse } from "next/server";
+
+export async function GET() {
+	// if user is not authenticated, redirect them to login
+	const userData: UserDisplayData | null = await getCurrentSession();
+	if (userData === null) {
+		redirect("/user/login");
+	} else {
+		await logoutUserAction(userData);
+		console.log("logged out");
+		redirect("/user/login");
+	}
+}
