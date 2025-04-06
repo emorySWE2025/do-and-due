422bddf374b0fd9bacf9c0e35ea94bf2720dcc2e
beatrice <bbrownmulry@gmail.com>
2025-04-06 14:50:27 -0400

    fix subsequent login bug where, if the logout route.ts redirected to login, the browser would immediately log out after logging in the next time

--- frontend/app/user/logout/route.ts
+++ frontend/app/user/logout/route.ts
@@ -1,16 +1,9 @@
-import { getCurrentSession, logoutUserAction } from "@/actions/users.server";
-import { UserDisplayData } from "@/schemas/fe.schema";
+import { logoutUserAction } from "@/actions/users.server";
 import { redirect } from "next/navigation";
-import { NextResponse } from "next/server";
 
 export async function GET() {
-	// // if user is not authenticated, redirect them to login
-	// const userData: UserDisplayData | null = await getCurrentSession();
-	// if (userData === null) {
-	// 	redirect("/user/login");
-	// } else {
-	await logoutUserAction();
-	console.log("logged out");
-	redirect("/user/login");
-	// }
+	await logoutUserAction().then(() => {
+		console.log("logged out");
+		redirect("/");
+	});
 }
