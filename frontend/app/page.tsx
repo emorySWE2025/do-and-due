248551b2795942be4b7e565a6302b8e487e620d1
beatrice <bbrownmulry@gmail.com>
2025-04-06 14:05:27 -0400

    add placeholder data back for testing

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -4,6 +4,117 @@ import PageLayout from "@/components/PageLayout";
 import { UserDisplayData } from "@/schemas/fe.schema";
 import { redirect } from "next/navigation";
 
+const placeholderUser: UserDisplayData = {
+	id: 1,
+	username: "bea-bm",
+	email: "test@test.com",
+	photoPath: "/profile-placeholder.png",
+	groups: [
+		{
+			id: 101,
+			name: "Home 🏠︎",
+			events: [
+				{
+					name: "Vacuum the living room",
+					date: "2025-03-26",
+					completed: true,
+				},
+				{
+					name: "Do the laundry",
+					date: "2025-03-26",
+					completed: true,
+				},
+				{
+					name: "Wash the car",
+					date: "2025-03-27",
+					completed: true,
+				},
+				{
+					name: "Take out recycling",
+					date: "2025-03-27",
+					completed: true,
+				},
+				{
+					name: "Dust shelves and furniture",
+					date: "2025-03-28",
+					completed: true,
+				},
+				{
+					name: "Mop the kitchen floor",
+					date: "2025-03-28",
+					completed: false,
+				},
+				{
+					name: "Clean the cat's water fountain",
+					date: "2025-03-29",
+					completed: false,
+				},
+				{
+					name: "Organize the pantry",
+					date: "2025-03-29",
+					completed: false,
+				},
+				{
+					name: "Water the plants",
+					date: "2025-03-30",
+					completed: false,
+				},
+				{
+					name: "Deep clean the bathroom",
+					date: "2025-03-30",
+					completed: false,
+				},
+				{
+					name: "Change bed sheets",
+					date: "2025-03-31",
+					completed: false,
+				},
+			],
+		},
+		{
+			id: 102,
+			name: "Chicago Trip 🏙️",
+			events: [
+				{
+					name: "Take out the trash",
+					date: "2025-03-29",
+					completed: false,
+				},
+				{
+					name: "Clean the kitchen",
+					date: "2025-03-30",
+					completed: false,
+				},
+				{
+					name: "Wipe down bathroom counters",
+					date: "2025-03-30",
+					completed: false,
+				},
+				{
+					name: "Sweep entryway",
+					date: "2025-03-31",
+					completed: false,
+				},
+				{
+					name: "Pack up luggage",
+					date: "2025-03-31",
+					completed: false,
+				},
+				{
+					name: "Clean AirBNB before checkout",
+					date: "2025-04-01",
+					completed: false,
+				},
+				{
+					name: "Check for forgotten items",
+					date: "2025-04-01",
+					completed: false,
+				},
+			],
+		},
+	],
+};
+
 export default async function Home() {
 	// if user is not authenticated, redirect them to login
 	const userData: UserDisplayData | null = await getCurrentSession();
@@ -13,7 +124,7 @@ export default async function Home() {
 
 	return (
 		<PageLayout>
-			<HomeFrame userData={userData} />
+			<HomeFrame userData={placeholderUser} />
 		</PageLayout>
 	);
 }
