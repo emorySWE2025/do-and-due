b0cc4b93843d1b4df9ce663625dd89b401d7f5cb
beatrice <bbrownmulry@gmail.com>
2025-04-20 17:44:55 -0400

    initialize placeholder page.tsx files for /events routes

---
+++ frontend/app/events/[eventId]/page.tsx
@@ -0,0 +1,19 @@
+import { getCurrentSession } from "@/actions/users.server";
+import PageLayout from "@/components/PageLayout";
+import { UserDisplayData } from "@/schemas/fe.schema";
+import { redirect } from "next/navigation";
+import GroupsFrame from "@/components/GroupsFrame";
+
+export default async function GroupsPage() {
+	// if user is not authenticated, redirect them to login
+	const userData: UserDisplayData | null = await getCurrentSession();
+	if (userData === null) {
+		redirect("/user/login");
+	}
+
+	return (
+		<PageLayout>
+			<GroupsFrame GroupsData={userData.groups} />
+		</PageLayout>
+	);
+}
