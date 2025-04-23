bef7ce9ca6d07a4337bb30f6ad4cceee331e18f2
ianjorin <israelwhiz@gmail.com>
2025-04-22 21:48:20 -0400

    adding ui that list members of a group

--- frontend/components/groups-page/GroupsFrame.tsx
+++ frontend/components/groups-page/GroupsFrame.tsx
@@ -11,17 +11,16 @@ export default function GroupsFrame({
 }) {
 	return (
 		// map through group data to card component
-		<div className="m-auto mt-16 h-max w-max max-w-5xl space-y-8 rounded-lg border-[1px] border-gray-300 p-16 shadow-sm">
-			<h1 className="text-3xl font-bold">Groups</h1>
-			<div className="flex flex-row flex-nowrap gap-8 overflow-x-auto">
+			<div className="text-center p-4 mx-auto">
+			<h1 className="text-3xl font-bold mb-8">Groups</h1>
+			<div className="flex flex-row flex-nowrap  gap-8 overflow-x-auto">
 				{GroupsData.map((group) => (
 					<GroupCard
 						key={group.id}
 						groupData={group}
-						onView={() => {}}
-					/>
+						onView={() => { } } />
 				))}
 			</div>
-		</div>
+			</div>
 	);
 }
